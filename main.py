9b4d9a5ce53c3bb1d19477896c8cc90238568f91
arthur125 <arthur.blackcold13@gmail.com>
2026-05-04 12:12:54 -0300

    Adiciona o menu

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello world!')
+# Sistema de opção do cadastro de pets
+while True:
+    print('Sistema de Cadastro de Adoção de Pets')
+
+    print('1. Cadastrar animal')
+    print('2. Ver animais')
+    print('3. Sair')
+
+    opcao = input('Escolha a opção desejada: ')
