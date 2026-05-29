e68c78255af83bcd36ba8fe70adfb25456698dd1
arthur125 <arthur.blackcold13@gmail.com>
2026-05-29 09:16:16 -0300

    Adiciona função clear_screen()

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print("""    _                             _                   
+# Declaração das variáveis globais
+import os
+
+opcao = 0
+clientes = []
+animais = []
+adocoes = []
+
+print('''                                                       
 / ___|(_)___| |_ ___ _ __ ___   __ _    __| | ___                    
 \___ \| / __| __/ _ \ '_ ` _ \ / _` |  / _` |/ _ \                   
  ___) | \__ \ ||  __/ | | | | | (_| | | (_| |  __/                   
@@ -8,20 +16,27 @@ print("""    _                             _
   / _ \ / _` |/ _ \ / __/ _` |/ _ \   / _` |/ _ \ | |_) / _ \ __/ __|
  / ___ \ (_| | (_) | (_| (_| | (_) | | (_| |  __/ |  __/  __/ |_\__ \\
 /_/   \_\__,_|\___/ \___\__,_|\___/   \__,_|\___| |_|   \___|\__|___/
-                                                                 """)
+                                                                    ''')
+
+# Função para limpar a tela
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 # Sistema de opção do cadastro de pets
 while True:
     print('Sistema de Cadastro de Adoção de Pets')
 
-    print('1. Cadastrar animal')
-    print('2. Ver animais')
-    print('3. Sair')
+    print('1. Cliente')
+    print('2. Animal')
+    print('3. Adoção')
+    print('4. Sair')
 
     opcao = input('Escolha a opção desejada: ')
 
     # Sistema de descrição do animal
     if opcao == '1':
-        input('Digite a idade do animal: ')
-        input('Digite o tamanho do animal (cm): ')
-        input('Digite o peso do animal (kg): ')
+        clear_screen()
+        print('1. Cadastrar animal')
+        print('2. Ver animais')
+        print('3. Voltar')
+        opcao = input('Escolha a opção desejada:')
